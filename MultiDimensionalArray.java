e0dba137032a6a2dbbdcd59ecadf8a45940e4b0c
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-01 20:08:21 +0300

    Create MultiDimensionalArray.java

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+////        System.out.println(Arrays.toString(twoDarray[1]));
+//
+////        System.out.println(Arrays.deepToString(array));
+////        System.out.println(twoDarray.length);
+////        for (int i = 0; i <twoDarray.length ; i++) {
+////            System.out.println(Arrays.toString(twoDarray[i]));
+////        }
+//
+//        int[][] twoDarray = new int[][]{{1, 3, 5}, {1, 4}};
+//
+////        for (int i = 0; i < twoDarray.length; i++) {
+////            for (int j = 0; j < twoDarray[i].length; j++) {
+////                System.out.println(twoDarray[i][j]);
+////            }
+////        }
+//
+//        for (int i = 0; i < twoDarray.length; i++) {
+////            twoDarray[i]
+//            for (int j = 0; j < twoDarray[i].length; j++) {
+////                STEPS
+////          twoDarray[0][0];
+////          twoDarray[0][1];
+////          twoDarray[0][2];
+//
+////          twoDarray[1][0]
+////          twoDarray[1][1]
+//
+////                System.out.println(twoDarray[i][j]);
+//            }
+//        }
+//
+////        int [] x = {1,3,7,1,5};
+////        System.out.println(x);
+
+
+//        int[][] twoDarray = new int[][]{{1, 3, 5}, {1, 4}};
+//        for (int i = 0; i <twoDarray.length ; i++) {
+//            for (int j = 0; j < twoDarray[i].length; j++) {
+//                System.out.println(twoDarray[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+
+//        int [] array = new int [5];
+//        // {{0,0,0},{0,0,0},{0,0,0}}
+//        int [][] matrix = new int [3][3];
+        int[][] matrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        char[][] matrixString = {{'*','*', '*'}, {'*', '*', '*'}, {'*', '*', '*'}};
+        
+//        System.out.println(Arrays.deepToString(matrix));
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.println(matrix[i][j]);
+            }
+        }
+
+    }
+}
